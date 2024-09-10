ca15da422f8873536a8e98c7c10abf92d10db2f5
Daniela <danielasouza0729@gmail.com>
2024-09-10 20:58:41 -0300

    Implementações

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 function pesquisar() {
     /* Seleciona a seção onde os resultados da pesquisa serão exibidos */
     let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
     
+    //se campoPesquisa for uma string sem nada
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Essa palavra não foi encontrada.Utilize palavras chaves para identificar sua dúvida </p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
 
     /* Inicializa uma string vazia para armazenar o HTML dos resultados */
     let perguntas_frequentes = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     /* Itera sobre cada pergunta e gera o HTML correspondente */
     for (let dado of perguntas) {
+        titulo = dado.titulo.toLowerCase()
+         descricao = dado.descricao.toLowerCase()
+         tags = dado.tags.toLowerCase()
+        
+        // se titulo includes campoPesquisa
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+        //cria um novo elemento
         perguntas_frequentes += `
-            <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">${dado.titulo}</a>
-                </h2>
-                <p class="descricao-meta">${dado.descrição}</p>
-                <a href=${dado.link} target="_blank">Para mais informações</a>
-            </div>
-        `;
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Para mais informações</a>
+        </div>
+    `;
+        }
+      
+    }
+
+    if(!perguntas_frequentes){
+        perguntas_frequentes = "<p>Nada foi encontrado</p>"
+
     }
 
     /* Atualiza o conteúdo HTML da seção com os resultados */
--- base_perguntas.js
+++ base_perguntas.js
@@ -2,98 +2,118 @@ let perguntas = [
     
     {
         titulo: "O doador sente dor na coleta da medula?",
-        descrição: "A coleta da medula óssea é realizada sob anestesia, por isso, o doador nãosente dor durante o procedimento.Após a cirurgia, é comum sentir um pouco de dor no local da punção, mas essa dor pode ser controlada com analgésicos.",
+        descricao: "A coleta da medula óssea é realizada sob anestesia, por isso, o doador nãosente dor durante o procedimento.Após a cirurgia, é comum sentir um pouco de dor no local da punção, mas essa dor pode ser controlada com analgésicos.",
         link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_de_medula, procedimento, dor, anestesia, puncao"
     },
     {
         titulo: "Quais os requisitos para ser um doador de medula óssea?",
-        descrição: "Ter entre 18 e 35 anos(faixa etária mais comum para doadores, mas o cadastro é válido até os 60 anos); estar em bom estado de saúde (necessário passar por uma avaliação médica para verificar se você está apto a doar); realizar um cadastro, o cadastro é feito em um hemocentro, onde é coletada uma amostra de sanguepara a tipagem HLAque serve para identificar a compatibilidade com possíveis receptores",
+        descricao: "Ter entre 18 e 35 anos(faixa etária mais comum para doadores, mas o cadastro é válido até os 60 anos); estar em bom estado de saúde (necessário passar por uma avaliação médica para verificar se você está apto a doar); realizar um cadastro, o cadastro é feito em um hemocentro, onde é coletada uma amostra de sanguepara a tipagem HLAque serve para identificar a compatibilidade com possíveis receptores",
         link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_de_medula, requisitos_doador, idade, saude, cadastro, tipagem_HLA"
     },
     {
         titulo: "Quanto tempo de internação e recuperação para o doador?",
-        descrição: "O tempo de internação após a doação de medula óssea varia, mas geralmente é de 24 horas. A recuperação completa pode levar alguns dias, e é importante seguir as orientações médicas paragarantir uma boa recuperação",
+        descricao: "O tempo de internação após a doação de medula óssea varia, mas geralmente é de 24 horas. A recuperação completa pode levar alguns dias, e é importante seguir as orientações médicas paragarantir uma boa recuperação",
         link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, internacao, recuperacao, tempo_recuperacao, pos_doacao"
+
     },
     {
         titulo: "Qual a importância da doação de medula óssea?",
-        descrição: "A doação de medula óssea pode salvar a vida de pessoas com doenças como leucemia e outras doenças do sangue. Ao doar, você oferece uma nova chance de vida para alguém que precisa.",
-        link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/"
+        descricao: "A doação de medula óssea pode salvar a vida de pessoas com doenças como leucemia e outras doenças do sangue. Ao doar, você oferece uma nova chance de vida para alguém que precisa.",
+        link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, importancia, salvar_vidas, leucemia, doencas_hematologicas"
       },
       {
         titulo: "Quais os cuidados após a doação?",
-        descrição: "Após a doação, é importante descansar, evitar atividades físicas intensas e seguir as orientações médicas.",
+        descricao: "Após a doação, é importante descansar, evitar atividades físicas intensas e seguir as orientações médicas.",
         link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, pos_doacao, cuidados_pos_doacao, descanso, atividade_fisica"
       },
       {
         titulo: "Quais os tipos de coleta de medula óssea?",
-        descrição: "Existem dois principais métodos: a punção, onde as células são retiradas diretamente do osso da bacia, e a aférese, onde as células são coletadas do sangue após um estímulo para que migrem para a circulação.",
+        descricao: "Existem dois principais métodos: a punção, onde as células são retiradas diretamente do osso da bacia, e a aférese, onde as células são coletadas do sangue após um estímulo para que migrem para a circulação.",
         link: "https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, metodos_coleta, puncao, afere, celulas_tronco"
       },
       {
         titulo: "A doação de medula óssea interfere na vida pessoal e profissional?",
-        descrição: "A doação de medula óssea geralmente não interfere na vida pessoal e profissional. A recuperação é rápida e a maioria das pessoas retorna às suas atividades normais em poucos dias.",
+        descricao: "A doação de medula óssea geralmente não interfere na vida pessoal e profissional. A recuperação é rápida e a maioria das pessoas retorna às suas atividades normais em poucos dias.",
         link: "https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags: "doacao_medula,impacto_vida,vida_pessoal,vida_profissional,recuperacao,retorno_atividades"
       },
       {
         titulo: "A doação de medula óssea é um procedimento seguro?",
-        descrição: "Sim, a doação de medula óssea é um procedimento seguro quando realizado por profissionais qualificados em um ambiente hospitalar adequado. Os riscos são mínimos e comparáveis aos de outras cirurgias menores.",
+        descricao: "Sim, a doação de medula óssea é um procedimento seguro quando realizado por profissionais qualificados em um ambiente hospitalar adequado. Os riscos são mínimos e comparáveis aos de outras cirurgias menores.",
         link: "https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, seguranca, procedimento_medico, riscos, complicacoes, hospital"
       },
       {
         titulo: "Posso doar medula óssea se tiver tatuagem ou piercing?",
-        descrição: "Sim, geralmente não há impedimento para doar medula óssea se você tiver tatuagem ou piercing. No entanto, é importante informar ao hemocentro sobre essas informações.",
+        descricao: "Sim, geralmente não há impedimento para doar medula óssea se você tiver tatuagem ou piercing. No entanto, é importante informar ao hemocentro sobre essas informações.",
         link: "https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, requisitos_doador, tatuagem, piercing, restricoes_doacao"
       },
       {
         titulo: "Quais os riscos da doação de medula óssea?",
-        descrição: "Os riscos da doação são baixos e geralmente se limitam a desconforto no local da punção, que pode ser controlado com analgésicos. Complicações mais graves são raras.",
-        link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/"
+        descricao: "Os riscos da doação são baixos e geralmente se limitam a desconforto no local da punção, que pode ser controlado com analgésicos. Complicações mais graves são raras.",
+        link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, riscos, complicacoes, puncao, anestesia"
       },
       {
         titulo: "Posso doar medula óssea mais de uma vez?",
-        descrição: "Sim, é possível doar medula óssea mais de uma vez, desde que o doador esteja apto e haja necessidade.",
-        link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/"
+        descricao: "Sim, é possível doar medula óssea mais de uma vez, desde que o doador esteja apto e haja necessidade.",
+        link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, multiplas_doacoes, aptidao_doador"
       },
       {
         titulo: "A doação de medula óssea é paga?",
-        descrição: "Não, a doação de medula óssea é um ato voluntário e altruísta. Não há nenhum tipo de pagamento envolvido no processo.",
-        link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/"
+        descricao: "Não, a doação de medula óssea é um ato voluntário e altruísta. Não há nenhum tipo de pagamento envolvido no processo.",
+        link:"https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, pagamento, voluntariado, altruismo"
       },
       {
         titulo: "Quanto tempo leva o processo de doação?",
-        descrição: "O tempo do processo pode variar, desde o cadastro até a possível doação. No entanto, a coleta da medula óssea em si costuma durar algumas horas.",
-        link:"https://redome.inca.gov.br/doador/como-se-tornar-um-doador/"
+        descricao: "O tempo do processo pode variar, desde o cadastro até a possível doação. No entanto, a coleta da medula óssea em si costuma durar algumas horas.",
+        link:"https://redome.inca.gov.br/doador/como-se-tornar-um-doador/",
+        tags:"doacao_medula, tempo_processo, cadastro_doador, coleta_medula"
       },
       {
         titulo: "O doador recebe algum tipo de pagamento pela doação?",
-        descrição: "Não, a doação de medula óssea é um ato voluntário e altruísta. O doador não recebe nenhum tipo de pagamento pela sua contribuição.",
+        descricao: "Não, a doação de medula óssea é um ato voluntário e altruísta. O doador não recebe nenhum tipo de pagamento pela sua contribuição.",
         link: "https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, remuneracao, voluntariado, altruismo"
       },
       {
         titulo: "Meu nome será divulgado após a doação?",
-        descrição: "A identidade do doador é mantida em sigilo, a menos que ambos, doador e receptor, autorizem o contato. Seu nome não será divulgado para o receptor sem a sua autorização.",
+        descricao: "A identidade do doador é mantida em sigilo, a menos que ambos, doador e receptor, autorizem o contato. Seu nome não será divulgado para o receptor sem a sua autorização.",
         link: "https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, sigilo, privacidade, doador_receptor"
       },
       {
         titulo: "E se eu me arrepender da doação?",
-        descrição: "É importante refletir bem antes de tomar a decisão de doar, pois uma vez iniciada a coleta, não é possível interromper o processo. No entanto, você pode retirar o seu consentimento a qualquer momento antes do início da coleta.",
+        descricao: "É importante refletir bem antes de tomar a decisão de doar, pois uma vez iniciada a coleta, não é possível interromper o processo. No entanto, você pode retirar o seu consentimento a qualquer momento antes do início da coleta.",
         link: "https://redome.inca.gov.br/doador/a-doacao-de-medula-ossea/",
+        tags:"doacao_medula, arrependimento, consentimento, coleta_medula"
       },
       {
         titulo: "Onde posso me cadastrar para ser um doador de medula óssea?",
-        descrição: "Você pode se cadastrar como doador de medula óssea em hemocentros, hospitais com programas de transplante ou através do site do REDOME. Consulte o hemocentro mais próximo para obter informações sobre os locais de cadastro.",
-        link: "https://redome.inca.gov.br/doador/como-se-tornar-um-doador/"
+        descricao: "Você pode se cadastrar como doador de medula óssea em hemocentros, hospitais com programas de transplante ou através do site do REDOME. Consulte o hemocentro mais próximo para obter informações sobre os locais de cadastro.",
+        link: "https://redome.inca.gov.br/doador/como-se-tornar-um-doador/",
+        tags:"doacao_medula, cadastro, hemocentro, hospital, redome"
       },
       {
         titulo: "O que é o REDOME?",
-        descrição: "O REDOME é o Registro Nacional de Doadores Voluntários de Medula Óssea. É um banco de dados que armazena informações de pessoas dispostas a doar medula óssea para pacientes que precisam de transplante. O REDOME conecta doadores e receptores compatíveis.",
-        link: "https://redome.inca.gov.br/institucional/quem-somos/"
+        descricao: "O REDOME é o Registro Nacional de Doadores Voluntários de Medula Óssea. É um banco de dados que armazena informações de pessoas dispostas a doar medula óssea para pacientes que precisam de transplante. O REDOME conecta doadores e receptores compatíveis.",
+        link: "https://redome.inca.gov.br/institucional/quem-somos/",
+        tags:"redome, registro_doadores, medula_ossea, transplante"
       },
       {
         titulo: "Qual a importância do REDOME?",
-        descrição: "O REDOME é fundamental para aumentar as chances de encontrar um doador compatível para pacientes que precisam de um transplante de medula óssea. Ao cadastrar-se no REDOME, você contribui para salvar vidas.",
-        link: "https://redome.inca.gov.br/institucional/o-que-faz/"
+        descricao: "O REDOME é fundamental para aumentar as chances de encontrar um doador compatível para pacientes que precisam de um transplante de medula óssea. Ao cadastrar-se no REDOME, você contribui para salvar vidas.",
+        link: "https://redome.inca.gov.br/institucional/o-que-faz/",
+        tags:"redome, importancia, transplante, salvar_vidas"
       },
     ];
 
